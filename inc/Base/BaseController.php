b95b07ec8096cafcab318ae320814615ef3fab74
lenya_official <amlen.ya@mail.ru>
2026-03-21 11:22:31 +0200

    Урок 21. Оптимизация

--- inc/Base/BaseController.php
+++ inc/Base/BaseController.php
@@ -12,9 +12,23 @@ class BaseController
 
 	public $plugin;
 
+	public $managers = array();
+
 	public function __construct() {
 		$this->plugin_path = plugin_dir_path( dirname( __FILE__, 2 ) );
 		$this->plugin_url = plugin_dir_url( dirname( __FILE__, 2 ) );
 		$this->plugin = plugin_basename( dirname( __FILE__, 3 ) ) . '/amlen-plugin.php';
+
+		$this->managers = array(
+			'cpt_manager' => 'CPT Manager',
+			'taxonomy_manager' => 'Taxonomy Manager',
+			'media_manager' => 'Media Manager',
+			'gallery_manager' => 'Gallery Manager',
+			'testimonial_manager' => 'Testimonial Manager',
+			'templates_manager' => 'Templates Manager',
+			'login_manager' => 'Ajax Login/Signup',
+			'membership_manager' => 'Membership Manager',
+			'chat_manager' => 'Chat Manager',
+		);
 	}
 }
--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -88,53 +88,15 @@ class Admin extends BaseController
 
 	public function setSettings()
 	{
-		$args = array(
-			array(
+		$args = array();
+
+		foreach ($this->managers as $key => $value) {
+			$args[] = array(
 				'option_group' => 'amlen_plugin_settings',
-				'option_name' => 'cpt_manager',
+				'option_name' => $key,
 				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			),
-			array(
-				'option_group' => 'amlen_plugin_settings',
-				'option_name' => 'taxonomy_manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			),
-			array(
-				'option_group' => 'amlen_plugin_settings',
-				'option_name' => 'media_widgets',
-				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			),
-			array(
-				'option_group' => 'amlen_plugin_settings',
-				'option_name' => 'gallery_manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			),
-			array(
-				'option_group' => 'amlen_plugin_settings',
-				'option_name' => 'testimonial_manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			),
-			array(
-				'option_group' => 'amlen_plugin_settings',
-				'option_name' => 'templates_manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			),
-			array(
-				'option_group' => 'amlen_plugin_settings',
-				'option_name' => 'login_manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			),
-			array(
-				'option_group' => 'amlen_plugin_settings',
-				'option_name' => 'membership_manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			),
-			array(
-				'option_group' => 'amlen_plugin_settings',
-				'option_name' => 'chat_manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			)
-		);
+			);
+		}
 
 		$this->settings->setSettings( $args );
 	}
@@ -155,107 +117,21 @@ class Admin extends BaseController
 
 	public function setFields()
 	{
-		$args = array(
-			array(
-				'id' => 'cpt_manager',
-				'title' => 'CPT Manager',
+		$args = array();
+
+		foreach ($this->managers as $key => $value) {
+			$args[] = array(
+				'id' => $key,
+				'title' => $value,
 				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
 				'page' => 'amlen_plugin',
 				'section' => 'amlen_admin_index',
 				'args' => array(
-					'label_for' => 'cpt_manager',
+					'label_for' => $key,
 					'class' => 'ui-toggle'
 				)
-			),
-			array(
-				'id' => 'taxonomy_manager',
-				'title' => 'Taxonomy Manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
-				'page' => 'amlen_plugin',
-				'section' => 'amlen_admin_index',
-				'args' => array(
-					'label_for' => 'taxonomy_manager',
-					'class' => 'ui-toggle'
-				)
-			),
-			array(
-				'id' => 'media_widgets',
-				'title' => 'Media Widgets',
-				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
-				'page' => 'amlen_plugin',
-				'section' => 'amlen_admin_index',
-				'args' => array(
-					'label_for' => 'media_widgets',
-					'class' => 'ui-toggle'
-				)
-			),
-			array(
-				'id' => 'gallery_manager',
-				'title' => 'Gallery Manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
-				'page' => 'amlen_plugin',
-				'section' => 'amlen_admin_index',
-				'args' => array(
-					'label_for' => 'gallery_manager',
-					'class' => 'ui-toggle'
-				)
-			),
-			array(
-				'id' => 'testimonial_manager',
-				'title' => 'Testimonial Manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
-				'page' => 'amlen_plugin',
-				'section' => 'amlen_admin_index',
-				'args' => array(
-					'label_for' => 'testimonial_manager',
-					'class' => 'ui-toggle'
-				)
-			),
-			array(
-				'id' => 'templates_manager',
-				'title' => 'Templates Manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
-				'page' => 'amlen_plugin',
-				'section' => 'amlen_admin_index',
-				'args' => array(
-					'label_for' => 'templates_manager',
-					'class' => 'ui-toggle'
-				)
-			),
-			array(
-				'id' => 'login_manager',
-				'title' => 'Login Manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
-				'page' => 'amlen_plugin',
-				'section' => 'amlen_admin_index',
-				'args' => array(
-					'label_for' => 'login_manager',
-					'class' => 'ui-toggle'
-				)
-			),
-			array(
-				'id' => 'membership_manager',
-				'title' => 'Membership Manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
-				'page' => 'amlen_plugin',
-				'section' => 'amlen_admin_index',
-				'args' => array(
-					'label_for' => 'membership_manager',
-					'class' => 'ui-toggle'
-				)
-			),
-			array(
-				'id' => 'chat_manager',
-				'title' => 'Chat Manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
-				'page' => 'amlen_plugin',
-				'section' => 'amlen_admin_index',
-				'args' => array(
-					'label_for' => 'chat_manager',
-					'class' => 'ui-toggle'
-				)
-			)
-		);
+			);
+		}
 
 		$this->settings->setFields( $args );
 	}
