5f6501e98c6cfaadde5f57f55b0eadfde68e9277
lenya_official <amlen.ya@mail.ru>
2026-03-23 14:18:03 +0200

    Урок №23. Фикс активации плагина для нового пользователя

--- inc/Base/Activate.php
+++ inc/Base/Activate.php
@@ -8,5 +8,13 @@ class Activate
 {
 	public static function activate() {
 		flush_rewrite_rules();
+
+		if ( get_option( 'amlen_plugin' ) ) {
+			return;
+		}
+
+		$default = array();
+
+		update_option( 'amlen_plugin', $default );
 	}
 }
